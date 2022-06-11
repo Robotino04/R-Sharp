eeeb3aa04ac815590974f9c2cdb824ba4cb31236
Robotino04 <100292446+Robotino04@users.noreply.github.com>
2022-06-12 00:45:15 +0200

    Added nasm code generation.

---
+++ compiler/include/R-Sharp/NASMCodeGenerator.hpp
@@ -0,0 +1,58 @@
+#pragma once
+
+#include "R-Sharp/AstVisitor.hpp"
+#include "R-Sharp/Syscall.hpp"
+
+#include <memory>
+#include <string>
+
+class NASMCodeGenerator : public AstVisitor {
+    public:
+        NASMCodeGenerator(std::shared_ptr<AstNode> root);
+
+        std::string generate();
+
+        void visit(AstProgram* node) override;
+        // void visit(AstParameterList* node) override;
+
+        void visit(AstFunction* node) override;
+        // void visit(AstFunctionDeclaration* node) override;
+
+        // void visit(AstBlock* node) override;
+        void visit(AstReturn* node) override;
+        // void visit(AstExpressionStatement* node) override;
+        // void visit(AstConditionalStatement* node) override;
+        // void visit(AstForLoopDeclaration* node) override;
+        // void visit(AstForLoopExpression* node) override;
+        // void visit(AstWhileLoop* node) override;
+        // void visit(AstDoWhileLoop* node) override;
+        // void visit(AstBreak* node) override;
+        // void visit(AstSkip* node) override;
+        // void visit(AstErrorStatement* node) override;
+
+        // void visit(AstUnary* node) override;
+        // void visit(AstBinary* node) override;
+        void visit(AstInteger* node) override;
+        // void visit(AstVariableAccess* node) override;
+        // void visit(AstVariableAssignment* node) override;
+        // void visit(AstConditionalExpression* node) override;
+        // void visit(AstFunctionCall* node) override;
+
+        // void visit(AstBuiltinType* node) override;
+        // void visit(AstTypeModifier* node) override;
+        // void visit(AstArray* node) override;
+
+        // void visit(AstVariableDeclaration* node) override;
+
+    private:
+        void indent();
+        void dedent();
+        
+        void emit(std::string const& str);
+        void emitIndented(std::string const& str);
+
+        void emitSyscall(Syscall callNr, std::string const& arg1="", std::string const& arg2="", std::string const& arg3="", std::string const& arg4="", std::string const& arg5="", std::string const& arg6="");
+
+        std::string source;
+        int indentLevel;
+};
---
+++ tools/syscall_list.py
@@ -0,0 +1,56 @@
+import requests
+import sys
+
+# read syscall table from https://github.com/torvalds/linux/blob/29dcea88779c856c7dc92040a0c01233263101d4/arch/x86/entry/syscalls/syscall_64.tbl
+url = 'https://raw.githubusercontent.com/torvalds/linux/29dcea88779c856c7dc92040a0c01233263101d4/arch/x86/entry/syscalls/syscall_64.tbl'
+
+# get the file
+r = requests.get(url)
+
+# split into lines
+lines = r.text.split('\n')
+
+pattern = """#pragma once
+
+enum class Syscall{{
+{}}};
+
+namespace std{{
+    inline std::string to_string(Syscall sc){{
+        switch(sc){{
+{}            default: return "UNKNOWN";
+        }}
+    }}
+}}
+"""
+
+outfile_name = "Syscall.hpp"
+if len(sys.argv) > 1:
+    outfile_name = sys.argv[1]
+else:
+    print(f"Generating syscall list")
+
+syscall_list = ""
+syscall_string_list = ""
+
+num_syscalls = 0
+for line in lines:
+    if line.startswith('#'): continue
+
+    entry_point = ""
+    if (len(line.split()) == 3):
+        number, type, name = line.split()
+    elif (len(line.split()) == 4):
+        number, type, name, entry_point = line.split()
+    else: continue
+
+    if (type == "common" or type == "64"):
+        syscall_list += f"     {name} = {number},\n"
+        syscall_string_list += f"            case Syscall::{name}: return \"{name}\";\n"
+        num_syscalls += 1
+
+print(f"Wrote {num_syscalls} syscalls to \"{outfile_name}\".")
+
+outfile = open(outfile_name, "w")
+outfile.write(pattern.format(syscall_list, syscall_string_list))
+outfile.close()
