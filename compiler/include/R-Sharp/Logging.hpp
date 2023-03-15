24caad87d8396eb625fa08ca1bbe5b8e8d724a61
Robotino04 <100292446+Robotino04@users.noreply.github.com>
2023-03-15 10:53:26 +0100

    Added ANSI library.
    Moved cmake test setup into test folder.

--- compiler/include/R-Sharp/Logging.hpp
+++ compiler/include/R-Sharp/Logging.hpp
@@ -5,41 +5,15 @@
 #include <list>
 #include <algorithm>
 
+#include "ANSI/ANSI.hpp"
+
 template<typename ...Args>
 inline void Fatal(Args&& ...args);
 
 namespace Internals{
-    const std::string ansiColorStart = "\033[";
-    const std::string ansiColorEnd = "m";
-
-    const std::string colorReset =         ansiColorStart + "0"  + ansiColorEnd;
-
-    const std::string colorBlack =         ansiColorStart + "30" + ansiColorEnd;
-    const std::string colorRed =           ansiColorStart + "31" + ansiColorEnd;
-    const std::string colorGreen =         ansiColorStart + "32" + ansiColorEnd;
-    const std::string colorYellow =        ansiColorStart + "33" + ansiColorEnd;
-    const std::string colorBlue =          ansiColorStart + "34" + ansiColorEnd;
-    const std::string colorMagenta =       ansiColorStart + "35" + ansiColorEnd;
-    const std::string colorCyan =          ansiColorStart + "36" + ansiColorEnd;
-    const std::string colorWhite =         ansiColorStart + "37" + ansiColorEnd;
-    const std::string colorBrightBlack =   ansiColorStart + "90" + ansiColorEnd;
-    const std::string colorBrightRed =     ansiColorStart + "91" + ansiColorEnd;
-    const std::string colorBrightGreen =   ansiColorStart + "92" + ansiColorEnd;
-    const std::string colorBrightYellow =  ansiColorStart + "93" + ansiColorEnd;
-    const std::string colorBrightBlue =    ansiColorStart + "94" + ansiColorEnd;
-    const std::string colorBrightMagenta = ansiColorStart + "95" + ansiColorEnd;
-    const std::string colorBrightCyan =    ansiColorStart + "96" + ansiColorEnd;
-    const std::string colorBrightWhite =   ansiColorStart + "97" + ansiColorEnd;
-
-    template <typename T>
-    void printToStream(std::ostream& stream, T&& val) {
-        stream << val;
-    }
-    
-    template <typename T, typename ...Args>
-    void printToStream(std::ostream& stream, T &&val, Args&& ...args) {
-        printToStream(stream, val);
-        printToStream(stream, args...);
+    template <typename ...Args>
+    void printToStream(std::ostream& stream, Args&& ...args) {
+        (stream << ... << args);
     }
 
     inline std::list<const char*> contextPath = {};
@@ -81,21 +55,21 @@ namespace Internals{
 
 template<typename ...Args>
 inline void Fatal(Args&& ...args){
-    Internals::printToStream(std::cout, Internals::colorRed, "[ERROR]", Internals::getContext(), ": ", Internals::colorReset, args..., '\n');
+    Internals::printToStream(std::cout, ANSI::set4BitColor(ANSI::Red), "[ERROR]", Internals::getContext(), ": ", ANSI::reset(), args..., '\n');
     exit(1);
 }
 template<typename ...Args>
 inline void Error(Args&& ...args){
     Internals::registerError();
-    Internals::printToStream(std::cout, Internals::colorRed, "[ERROR]", Internals::getContext(), ": ", Internals::colorReset, args..., '\n');
+    Internals::printToStream(std::cout, ANSI::set4BitColor(ANSI::Red), "[ERROR]", Internals::getContext(), ": ", ANSI::reset(), args..., '\n');
 }
 template<typename ...Args>
 inline void Warning(Args&& ...args){
-    Internals::printToStream(std::cout, Internals::colorYellow, "[WARNING]", Internals::getContext(), ": ", Internals::colorReset, args..., '\n');
+    Internals::printToStream(std::cout, ANSI::set4BitColor(ANSI::Yellow), "[WARNING]", Internals::getContext(), ": ", ANSI::reset(), args..., '\n');
 }
 template<typename ...Args>
 inline void Log(Args&& ...args){
-    Internals::printToStream(std::cout, Internals::colorBrightBlue, "[LOG]", Internals::getContext(), ": ", Internals::colorReset, args..., '\n');
+    Internals::printToStream(std::cout, ANSI::set4BitColor(ANSI::BrightBlue), "[LOG]", Internals::getContext(), ": ", ANSI::reset(), args..., '\n');
 }
 template<typename ...Args>
 inline void Print(Args&& ...args){
